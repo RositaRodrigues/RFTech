578077be9f510197ba3520c17e5a94ddc5864555
Rosita <roseoctober@hotmail.co.uk>
2016-07-16 15:20:24 +0100

    Initial design of search page. Added dummy data to search controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -52,5 +52,46 @@ angular.module('starter.controllers', [])
   ];
 })
 
+.controller('SearchCtrl', function($scope) {
+    $scope.add = add;
+    $scope.courses = [
+        {
+            title: "Multivariable Calculus",
+            code: "M2AA2",
+            year: "2015-16"
+        },
+        {
+          title: "Compilers",
+          code: "CO221",
+          year: "2014-15"
+        },
+        {
+          title: "Software Engineering Design",
+          code: "CO220",
+          year: "2015-16"
+        },
+        {
+          title: "Introduction to Artificial Intelligence",
+          code: "CO231",
+          year: "2014-15"
+        },
+        {
+          title: "Statistics",
+          code: "MJS2",
+          year: "2014-15"
+        },
+        {
+          title: "Operating Systems",
+          code: "CO211",
+          year: "2014-15"
+        }
+    ];
+
+    ////////
+    function add() {
+        window.alert("Added");
+    }
+})
+
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });

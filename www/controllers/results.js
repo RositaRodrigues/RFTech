bd4ed0f6d1342e0ad78aab28180b748904e19902
Rosita <roseoctober@hotmail.co.uk>
2016-07-17 01:47:39 +0100

    added database factory to store dummy data. dummy courses used in results.js from database factory

--- www/controllers/results.js
+++ www/controllers/results.js
@@ -1,38 +1,7 @@
 angular.module('starter')
-.controller('ResultsCtrl', function($scope) {
+.controller('ResultsCtrl', function($scope, Database) {
     $scope.add = add;
-    $scope.courses = [
-        {
-          title: "Multivariable Calculus",
-          code: "M2AA2",
-          year: "2015-16"
-        },
-        {
-          title: "Compilers",
-          code: "CO221",
-          year: "2014-15"
-        },
-        {
-          title: "Software Engineering Design",
-          code: "CO220",
-          year: "2015-16"
-        },
-        {
-          title: "Introduction to Artificial Intelligence",
-          code: "CO231",
-          year: "2014-15"
-        },
-        {
-          title: "Statistics",
-          code: "MJS2",
-          year: "2014-15"
-        },
-        {
-          title: "Operating Systems",
-          code: "CO211",
-          year: "2014-15"
-        }
-    ];
+    $scope.courses = Database.getCourses();
 
     ////////
     function add() {

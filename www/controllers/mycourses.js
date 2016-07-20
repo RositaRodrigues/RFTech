8ca853a07704ff2b617394ca9080008eeaabe317
Rosita <roseoctober@hotmail.co.uk>
2016-07-20 09:23:34 +0100

    added search bar in mycourses page. added links and changed format of courses in results page.

--- www/controllers/mycourses.js
+++ www/controllers/mycourses.js
@@ -1,7 +1,7 @@
 angular.module('starter')
   .controller('MyCoursesCtrl', function($scope, $stateParams, Database) {
     $scope.remove = remove;
-    $scope.courses = Database.getCourses();
+    $scope.courses = Database.getCourses(); // in future, will be user's courses
 
     ////////
     function remove(course) {

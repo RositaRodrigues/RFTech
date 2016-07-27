83c4a03336f718dfb34da7130ef320db8e6535cf
Jane Ossai <jo3g13@soton.ac.uk>
2016-07-28 00:44:39 +0100

    reviewsbyme not displaying

--- www/controllers/myReviews.js
+++ www/controllers/myReviews.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 
-.controller('MyReviewsCtrl', function($scope, Courses,Users,Settings) {
+.controller('MyReviewsCtrl', function($scope, Courses,Users) {
 
 $scope.todoAdd = function()
 {
@@ -9,41 +9,43 @@ $scope.todoAdd = function()
     var currentTime = date.getHours() + '.' + date.getMinutes();
     var currentDate = date.getDate() +'/'+ (date.getMonth()+1) + '/' + date.getFullYear();
 
-    $scope.courses = Courses.all();
+    $scope.reviews = Courses.getReviews();
+   
+    console.log('REVIEW length: '+ $scope.reviews.length);
+    console.log($scope.ds.name);
+    console.log($scope.ds.code);
 
-    console.log(($scope.courses.length));
-    console.log($scope.ds.courseName);
 
     //get a sample user I created
     Users.get('eggs11');
     $scope.user = Users.getUN('eggs11');
     //console.log( Users.getUN('eggs11'));
-
-
+ 
+    var id = Courses.getReviews().length;
     //TODO: change this to add a review to a courses review list - chnage courses schema first to allow this
     //a review being added to a list of course/reviews - courses
-    $scope.courses.push({id: (Courses.all().length),
-    coursecode:$scope.ds.courseCode ,
-    courseName: $scope.ds.courseName,
-    author: $scope.user.name,
-    comments: $scope.ds.comments,
-    rating: '3',
-    time: currentTime,
-    date: currentDate,
-    face:'img/identicon.png'});
+    console.log('id' + id);
 
-    console.log(Users.getUN('eggs11').reviewsByMe.length)
-    $scope.user.reviewsByMe.push({id: (Users.getUN('eggs11').reviewsByMe.length),
-    coursecode:$scope.ds.courseCode ,
-    courseName: $scope.ds.courseName,
-    author: $scope.user.name,
-    comments: $scope.ds.comments,
-    rating: '3',
-    time: currentTime,
-    date: currentDate,
-    face:'img/identicon.png'});
+    $scope.reviews.push({id: id,
+    courseCode:$scope.ds.code ,
+    courseName: $scope.ds.name,
+    author: $scope.user.userName,
+    comment: $scope.ds.comment,
+    rating: 4,
+    timeCreated: currentTime,
+    dateCreated: currentDate,
+    upvotes: 0,
+    downvotes: 0 
+    });
 
-    $scope.list = $scope.user.reviewsByMe;
+    //to check if review was added 
+    console.log(($scope.reviews.length));
+    
+    $scope.user.reviewsByMe.push({reviewID: id 
+    });
+
+
+    //$scope.list = $scope.user.reviewsByMe;
 
   };
 
@@ -53,16 +55,28 @@ $scope.todoAdd = function()
 
 .controller('RCtrl', function($scope, Courses,Users,$stateParams) {
    $scope.list = Users.getUN('eggs11').reviewsByMe;
+   //get the loggd in user's reviews //eggs11 is the user logged in 
+   $scope.list = Courses.getUserReview('eggs11');
+
 
 })
 
+//can use above controller review deatil  
 .controller('ReviewDetailCtrl', function($scope, $stateParams, Courses,Users) {
 
   //load first - seems to only work this way
-  $scope.myReview = Users.all();
-  $scope.myReview = Users.getUN('eggs11').reviewsByMe[$stateParams.reviewId];
-  console.log(Users.getUN('eggs11').reviewsByMe[$stateParams.reviewId]);
+
+  //$scope.myReview = Users.all();
+  $scope.myReview = Courses.getReviews();
+  $scope.revlist = Courses.getUserReview('eggs11');
+  $scope.myReview = $scope.revlist[$stateParams.reviewId];
+
+  //$scope.myReview = Users.all();
+  //$scope.myReview = Users.getUN('eggs11').reviewsByMe[$stateParams.reviewId];
+  //console.log(Users.getUN('eggs11').reviewsByMe[$stateParams.reviewId]);
   // $scope.de = Users.getList('eggs11',$stateParams.reviewId);
+  
+
   // console.log($scope.de[0]);
   // $scope.chat = $scope.de.reviewsByMe[$stateParams.reviewIds ];
 });
--- www/factories/dummyData.js
+++ www/factories/dummyData.js
@@ -3,67 +3,186 @@ angular.module('starter')
 .factory('Courses', function() {
   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var courses = [{
-    id: 0,
-    coursecode:'2322' ,
-    courseName: 'biology concepts',
-    author: 'Ben Sparrow',
-    comments: 'it was ok like?',
-    rating: '3',
-    time: '12:00',
-    date: '12/7/2016',
-    face:'img/ben.png'
 
-  }, {
-    id: 1,
-    author: 'Max Lynx',
-    coursecode:'2322' ,
-    courseName: 'stuffy concepts',
-    comments: 'You on your way?',
-    rating: '4',
-    time: '12:00',
-    date: '2/7/2016',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    author: 'Adam Bradleyson',
-    coursecode:'2322' ,
-    courseName: 'maths concepts',
-    comments: 'teacher was hilr ',
-    rating: '2',
-    time: '12:00',
-    date: '12/7/2016',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    author: 'Perry Governor',
-    coursecode:'2322' ,
-    courseName: 'science concepts',
-    comments: 'it was aight ?',
-    rating: '2',
-    time: '12:00',
-    date: '14/7/2016',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    author: 'Mike Harrington',
-    coursecode:'2322' ,
-    courseName: 'biology concepts',
-    comments: 'the course was ok , could have been beter doe',
-    rating: '5',
-    time: '12:00',
-    date: '12/7/2016',
-    face: 'img/mike.png'
-  }];
+    var courses = [
 
+     {
+      code:'comp233',
+       name:'Networks',
+       year:'2015-16',
+       uniCode:'102',
+       uniName:'University of Southampton',
+       link: 'http://google.co.uk' ,
+       about: 'Second from. Tree stars is fifth greater. Bring Third deep there without and given grass may hi'
+    
+     },
+
+       {
+        code:'comp3434',
+         name:'computational biology ',
+         year:'2015-16',
+         uniCode:'102',
+         uniName:'University of Southampton',
+         link: 'http://google.co.uk' ,
+         about: 'Second from. Tree stars is fifth greater. Bring Third deep there without and given grass may hi'
+         
+       },
+        {
+        code:'comp9090',
+         name:'algorithms  ',
+         year:'2014-15',
+         uniCode:'102',
+         uniName:'University of Southampton',
+         link: 'http://google.co.uk' ,
+         about: 'Second from. Tree stars is fifth greater. Bring Third deep there without and given grass may hi'
+         
+       },
+        {
+        code:'comp1234',
+         name:'history of computing  ',
+         year:'2015-16',
+         uniCode:'102',
+         uniName:'University of Southampton',
+         link: 'http://google.co.uk' ,
+         about: 'Second from. Tree stars is fifth greater. Bring Third deep there without and given grass may hi'
+         
+       },
+        {
+        code:'domp2828',
+         name:'distributed systems ',
+         year:'2013-14',
+         uniCode:'102',
+         uniName:'University of Southampton',
+         link: 'http://google.co.uk' ,
+         about: 'Second from. Tree stars is fifth greater. Bring Third deep there without and given grass may hi'
+         
+       }];
+       
+       var reviews = [
+       {
+         id: 0,
+         courseCode:"comp2828",
+         author: "eggs11", 
+         courseName:"distributed systems",
+         comment: "great course , great laugh as teacher was a clown . no really he was !",
+         dateCreated:"23/4/2016",
+         timeCreated: "12:00",
+         rating: 3, 
+         upvotes: 3, 
+         downvotes: 23
+       }
+
+    ];
+      
 
   return {
-    all: function() {
+    getCourses: function() {
       return courses;
     },
+    getReviews: function() {
+      return reviews;
+    },
+    getCourse: function(code){
+      for (var i = 0; i < courses.length; i++) {
+        if (courses[i].code === parseInt(code)) {
+          return courses[i]; //return the course witht that code  
+        }
+      }
+      return null;
+    },
+    getUserReview:function(username){
+      var list = [];
+      for (var i = 0; i < reviews.length; i++) 
+      {
+          if(reviews[i].author == username)
+          {
+            list.push(reviews[i]);
+            console.log('found the user');
+          }
+          else
+          {
+              console.log('user not found');
+
+          }
+      }
+      return list;
+    },
+    getCourseName: function(courseCode){
+      for (var i = 0; i < courses.length; i++) {
+        if (courses[i].code == courseCode) {
+          console.log('found it ');
+            return courses[i].name;
+        }
+        else{
+          console.log('cant find course with that course code');
+        }
+      }
+    },
+    upvote: function(reviewID) {
+      for (var i = 0; i < reviews.length; i++) {
+        if (reviews[i].id === parseInt(reviewID)) {
+              reviews[i].upvotes += 1;
+        }
+      }
+     
+    },
+    downvote: function(reviewID) {
+      for (var i = 0; i < reviews.length; i++) {
+        if (reviews[i].id === parseInt(reviewID)) {
+              reviews[i].downvotes -= 1;
+        }
+      }
+     
+    },
+    //from review list 
+    getReview: function(reviewID){
+        for (var i = 0; i < reviews.length; i++) {
+        if (reviews[i].id === parseInt(userID)) {
+              return reviews[i];
+        }
+      }
+    },
+    //for cousrse page 
+    //get all the reviews with this course code 
+    getCourseReviews: function(courseCode) {
+      var list = [];
+      for (var i = 0; i < reviews.length; i++) {
+        if (reviews[i].courseCode == courseCode) {
+         list.push(reviews[i]);
+          
+        }
+      }
+      return list;
+    },
+     generateUUID: function(){
+    var d = new Date().getTime();
+    if(window.performance && typeof window.performance.now === "function"){
+        d += performance.now(); //use high-precision timer if available
+    }
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+},
     remove: function(chat) {
       courses.splice(courses.indexOf(chat), 1);
+    },
+  
+   // add a review object to list of reviews
+   //dont need this , will use a factory in the controller to get all reviews then push the review to it :)
+    createReview: function(review) {
+      reviews.push(review);
+    },
+    //to be used for when crating a review we would calll this to get the user's full name to use for the author fiedl of a review
+    getUserFullName: function(userID) {
+      for (var i = 0; i < users.length; i++) {
+        if (usres[i].id === parseInt(userID)) {
+          return users[i].name;
+        }
+      }
+      return null;
     },
     get: function(chatId) {
       for (var i = 0; i < courses.length; i++) {
@@ -77,40 +196,40 @@ angular.module('starter')
 })
 
 
-.service('Settings', function () {
-    var settings = {
-    loggedIn: false,
-    user: "l"
-  };
+// .service('Settings', function () {
+//     var settings = {
+//     loggedIn: false,
+//     user: "l"
+//   };
 
-    return {
-    set: function(bool,username){
-    settings.loggedIn = bool;
-    settings.user = username;
-    console.log(bool + username);
-  },
-    all: function() {
-      return settings.loggedIn;
-    },
-    theUser: function() {
-      return settings.user;
-    },
-     name: function() {
-      return settings.user;
-    },
-    remove: function(user) {
-      users.splice(users.indexOf(user), 1);
-    },
-    get: function(userId) {
-      for (var i = 0; i < users.length; i++) {
-        if (users[i].id === parseInt(userId)) {
-          return users[i];
-        }
-      }
-      return null;
-    }
-  };
-})
+//     return {
+//     set: function(bool,username){
+//     settings.loggedIn = bool;
+//     settings.user = username;
+//     console.log(bool + username);
+//   },
+//     all: function() {
+//       return settings.loggedIn;
+//     },
+//     theUser: function() {
+//       return settings.user;
+//     },
+//      name: function() {
+//       return settings.user;
+//     },
+//     remove: function(user) {
+//       users.splice(users.indexOf(user), 1);
+//     },
+//     get: function(userId) {
+//       for (var i = 0; i < users.length; i++) {
+//         if (users[i].id === parseInt(userId)) {
+//           return users[i];
+//         }
+//       }
+//       return null;
+//     }
+//   };
+// })
 
 .factory('Users', function() {
   // Might use a resource here that returns a JSON array
@@ -129,7 +248,7 @@ angular.module('starter')
   }, {
     id: 1,
     userName:'eggs11' ,
-    name: 'Dan bowe',
+    name: 'Dan Bowe',
     uniName: 'Reading',
     reviewsByMe: [],
     bookmarks: [],
@@ -163,9 +282,9 @@ angular.module('starter')
       }
       return null;
     },
-      getUN: function(userId) {
+      getUN: function(username) {
       for (var i = 0; i < users.length; i++) {
-        if (users[i].userName == userId) {
+        if (users[i].userName == username) {
           return users[i];
         }
       }

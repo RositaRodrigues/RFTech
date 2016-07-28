454b964769416d0d4b9050bded450547e605ef9f
Rosita <roseoctober@hotmail.co.uk>
2016-07-28 21:16:33 +0100

    cleaned up some code. commented out code produces same result

--- www/controllers/reviews.js
+++ www/controllers/reviews.js
@@ -4,22 +4,22 @@ angular.module('starter')
     $scope.courseTitle = $stateParams.coursetitle;
     $scope.academicYear = $stateParams.academicyear;
 
-    var reviews = [];
-    // $scope.reviews = Database.getReviews();
     firebase.database().ref('reviews/'+$scope.courseCode).push(angular.copy(Database.getReview1()));
     firebase.database().ref('reviews/'+$scope.courseCode).push(angular.copy(Database.getReview2()));
 
+    $scope.reviews = [];
     firebase.database().ref('reviews/'+$scope.courseCode).once('value').then(function(snapshot) {
       snapshot.forEach(function(childSnapshot) {
         console.log(childSnapshot.val());
-        reviews.push(childSnapshot.val());
+        $scope.reviews.push(childSnapshot.val());
       });
     });
-    $scope.reviews = reviews;
-    console.log(reviews);
+
     // firebase.database().ref('reviews/'+$scope.courseCode).on('child_added', function(data) {
-    //   console.log(data.val());
+    //     console.log(data.val());
+    //     $scope.reviews.push(data.val());
     // });
+
     $scope.needsFullStar = function(number, rating) {
       return number <= rating;
     }

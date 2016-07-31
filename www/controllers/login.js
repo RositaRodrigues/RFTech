d40ce9c65ad4f4fe53c0e9261c11771d15216108
Rosita <roseoctober@hotmail.co.uk>
2016-07-31 02:40:15 +0100

    user authentication via firebase done. changed views for login page to simpler colours. done register page. currently not synced up with loginData code so search page not working

--- www/controllers/login.js
+++ www/controllers/login.js
@@ -1,23 +1,47 @@
 angular.module('starter')
 .controller('LoginCtrl', ['$scope', '$state', 'universityLogin', '$http', '$ionicHistory', function($scope, $state, university, $http, $ionicHistory) {
 
-  // Form data for the login page pulled from 'universityLogin' factory
-  $scope.loginData = university.getLoginData();
+  $scope.login = function(email, password) {
+    console.log("Hello");
+    console.log(email);
+    console.log(password);
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      var user = firebase.auth().currentUser;
+      console.log("Success");
+      console.log(user);
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    // TODO - check with firebase database whether user exists
-    var userExists = true; // DEFAULT will be false initially, true for testing purposes
-
-    // Routing to Search page
-    if(userExists) {
       $ionicHistory.nextViewOptions({
          disableBack: true
       });
       $state.go('app.search');
-    } else {
-      $scope.loginError = "Login error, try again."
-    }
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(error);
+    });
   };
 
-}]);
+
+
+
+  // // Form data for the login page pulled from 'universityLogin' factory
+  // $scope.loginData = university.getLoginData();
+  //
+  // // Perform the login action when the user submits the login form
+  // $scope.doLogin = function() {
+  //   // TODO - check with firebase database whether user exists
+  //   var userExists = true; // DEFAULT will be false initially, true for testing purposes
+  //
+  //   // Routing to Search page
+  //   if(userExists) {
+  //     $ionicHistory.nextViewOptions({
+  //        disableBack: true
+  //     });
+  //     $state.go('app.search');
+  //   } else {
+  //     $scope.loginError = "Login error, try again."
+  //   }
+  // };
+
+}])
--- www/controllers/register.js
+++ www/controllers/register.js
@@ -1,11 +1,33 @@
 angular.module('starter')
-.controller('RegisterCtrl', ['$scope', '$state', '$ionicSlideBoxDelegate', function($scope, $state, $ionicSlideBoxDelegate) {
+.controller('RegisterCtrl', function($scope, $ionicHistory, universityLogin) {
 
-  $scope.slideHasChanged = function($index){
-    alert('slideHasChanged $index=' + $index);
-    if($index === 0){
-      // first box
-    }
+  $scope.register = function(name, email, password) {
+    console.log("Hello");
+    console.log("name" + name);
+    console.log("email" + email);
+    console.log("password" + password);
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+      var user = firebase.auth().currentUser;
+      console.log("Success");
+      console.log(user);
+      $ionicHistory.nextViewOptions({
+         disableBack: true
+      });
+      $state.go('app.search');
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(error);
+    });
   };
 
-}])
+// .controller('RegisterCtrl', ['$scope', '$state', '$ionicSlideBoxDelegate', function($scope, $state, $ionicSlideBoxDelegate) {
+//
+//   $scope.slideHasChanged = function($index){
+//     alert('slideHasChanged $index=' + $index);
+//     if($index === 0){
+//       // first box
+//     }
+//   };
+
+})

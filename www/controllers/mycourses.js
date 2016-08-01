570e1dcf3ecfb9e7232a9ef8ea57e7a7f1e160fb
Rosita <roseoctober@hotmail.co.uk>
2016-08-01 07:58:47 +0100

    mycourses displaying correct hearts after removed/added from search

--- www/controllers/mycourses.js
+++ www/controllers/mycourses.js
@@ -3,15 +3,17 @@ angular.module('starter')
 
     var currentUser = $rootScope.currentUser;
 
-    $scope.courses = [];
-    firebase.database().ref(currentUser.university+'/mycourses/'+currentUser.uid).once('value').then(function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-        var courseID = childSnapshot.key;
+    $scope.$on('$ionicView.enter', function(e) {
+      $scope.courses = [];
+      firebase.database().ref(currentUser.university+'/mycourses/'+currentUser.uid).once('value').then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var courseID = childSnapshot.key;
 
-        firebase.database().ref(currentUser.university+'/courses/'+courseID).once('value').then(function(grandChildSnapshot) {
-            var course = grandChildSnapshot.val();
-            course.ID = courseID;
-            $scope.courses.push(course);
+          firebase.database().ref(currentUser.university+'/courses/'+courseID).once('value').then(function(grandChildSnapshot) {
+              var course = grandChildSnapshot.val();
+              course.ID = courseID;
+              $scope.courses.push(course);
+          });
         });
       });
     });

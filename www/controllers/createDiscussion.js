c1bd2b7b2ad14d9b75d80e833e2e6262a875033e
Rosita <roseoctober@hotmail.co.uk>
2016-08-01 08:42:46 +0100

    can now post anonymously

--- www/controllers/createDiscussion.js
+++ www/controllers/createDiscussion.js
@@ -4,25 +4,29 @@ angular.module('starter')
     $scope.courseTitle = $stateParams.coursetitle;
     $scope.academicYear = $stateParams.academicyear;
 
-        $scope.input = {}
+    $scope.input = {}
+    $scope.userName = $rootScope.currentUser.name;
 
-        $scope.postDiscussion = function() {
-          var date = new Date();
-          var min = date.getMinutes();
-          var currentTime = date.getHours() + ':' + (min<10?'0'+min:min);
-          var currentDate = date.getDate() +'/'+ (date.getMonth()+1) + '/' + date.getFullYear();
-          var user = $rootScope.currentUser;
+    $scope.postDiscussionAsAnonymous = function(postAsAnonymous) {
+      var date = new Date();
+      var min = date.getMinutes();
+      var currentTime = date.getHours() + ':' + (min<10?'0'+min:min);
+      var currentDate = date.getDate() +'/'+ (date.getMonth()+1) + '/' + date.getFullYear();
+      var user = $rootScope.currentUser;
+      var author = user.name;
+      if (postAsAnonymous) {
+        author = "Anonymous";
+      }
+      var newDiscussion = {
+        author: author,
+        dateCreated: currentDate,
+        description: $scope.input.description,
+        timeCreated: currentTime,
+        title: $scope.input.title,
+        posts: 0,
+        uid: user.uid
+      }
 
-          var newDiscussion = {
-            author: user.name,
-            dateCreated: currentDate,
-            description: $scope.input.description,
-            timeCreated: currentTime,
-            title: $scope.input.title,
-            posts: 0,
-            uid: user.uid
-          }
-
-          firebase.database().ref(user.university+'/discussions/'+$scope.courseCode).push(newDiscussion);
-        }
+      firebase.database().ref(user.university+'/discussions/'+$scope.courseCode).push(newDiscussion);
+    }
   });

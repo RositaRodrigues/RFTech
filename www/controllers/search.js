efb2b3191bfef2bac84ba987740686164a3d0ee8
Rosita <roseoctober@hotmail.co.uk>
2016-08-01 04:00:38 +0100

    got university to display in search back

--- www/controllers/search.js
+++ www/controllers/search.js
@@ -1,18 +1,9 @@
 angular.module('starter')
+.controller('SearchCtrl', ['$scope', '$stateParams', 'universityLogin', 'Database', '$rootScope', function($scope, $stateParams, university, Database, $rootScope) {
 
-  .controller('SearchCtrl', ['$scope', '$stateParams', 'universityLogin', 'Database', '$rootScope', function($scope, $stateParams, university, Database, $rootScope) {
-    // Determine university from input email
-    loginData = university.getLoginData();
-    university.getUniversity(loginData.univEmail).then(function(university) {
-        $scope.university = university;
-    });
-// =======
-//   .controller('SearchCtrl', ['$scope', '$stateParams', 'universityLogin', 'Database', '$rootScope', function($scope, $stateParams, university, Database, $rootScope) {
-// >>>>>>> master
+  var currentUser = $rootScope.currentUser;
 
-    var currentUser = $rootScope.currentUser; // needed?
-
-    $scope.university = currentUser.university;
-    // Get courses for autocomplete
-    $scope.courses = Database.getCourses();
-  }]);
+  $scope.university = currentUser.university;
+  // Get courses for autocomplete
+  $scope.courses = Database.getCourses();
+}]);
